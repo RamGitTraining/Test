91f7cc093e251d0016bdc979733b0f8fdaeadd3f
Ram <131741654+RamGitTraining@users.noreply.github.com>
2023-04-26 10:23:24 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -8,6 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
-  features {}
+  client_id                   = "00000000-0000-0000-0000-000000000000"
+  client_certificate          = var.client_certificate
+  client_certificate_password = var.client_certificate_password
+  tenant_id                   = "10000000-0000-0000-0000-000000000000"
+  subscription_id             = "20000000-0000-0000-0000-000000000000"
 }
