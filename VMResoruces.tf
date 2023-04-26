4f4ad4d6d85c10bd65ef3e00ffbe04ba3c4d8ad1
Ram <131741654+RamGitTraining@users.noreply.github.com>
2023-04-26 11:20:45 +0530

    Update VMResoruces.tf

--- VMResoruces.tf
+++ VMResoruces.tf
@@ -1,11 +1,11 @@
 resource "azurerm_virtual_machine" "vm1" {
-  name                  = "ramanarayananvm11"
+  name                  = "ramanarayananvm14"
   resource_group_name   = "github"
   location              = "East US"
   network_interface_ids = [azurerm_network_interface.nic1.id]
   vm_size               = "Standard_DS1_v2"
   storage_os_disk {
-    name = "vishnudisk1"
+    name = "vishnudis7k1"
     caching = "ReadWrite"
     managed_disk_type = "Standard_LRS"
     create_option = "FromImage"
@@ -37,7 +37,7 @@ resource "azurerm_virtual_machine" "vm1" {
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "shubham2361"
+  name                = "shubha7m2361"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "github"
@@ -45,14 +45,14 @@ resource "azurerm_virtual_network" "vnet1" {
 
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "vishnusubnet11"
+  name                 = "vishnusub6t11"
   resource_group_name  = "github"
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic1" {
-  name                = "vishnunic111"
+  name                = "vishnunic1611"
   location            = "East US"
   resource_group_name = "github"
 
@@ -65,7 +65,7 @@ resource "azurerm_network_interface" "nic1" {
 }
 
 resource "azurerm_public_ip" "public1" {
-  name = "vishnupublicip1611"
+  name = "rampublicip16711"
   resource_group_name = "github"
   location = "East US"
   allocation_method = "Static"
